5f441fa7be5a20e04a636a9580afd8e09acb7a70
archanVadgama <archan.vadgama@simformsolutions.com>
2025-04-04 18:51:13 +0530

    husky testing 3

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -19,7 +19,7 @@ export default defineConfig([
     extends: ["js/recommended", ...tseslint.configs.recommended],
     rules: {
       // 🚨 Possible Errors - Prevent syntax issues & runtime errors
-      "no-console": "warn",
+      "no-console": "error",
       "no-debugger": "warn",
       "no-unsafe-finally": "error",
       "no-extra-semi": "error",
